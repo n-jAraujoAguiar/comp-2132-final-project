92011ae7212fcec24fd816561cc31426222ff1a5
JAraujoAguiar <87349284+JAraujoAguiar@users.noreply.github.com>
2023-11-24 21:41:54 -0800

    other functions work
    added a few html elements
    added animation css
    added the function baselines

--- index/scripts/other-functions.js
+++ index/scripts/other-functions.js
@@ -1,1 +1,19 @@
 // review session 7
+
+// variables that store the DOM elements
+const htmlPlayButton = document.getElementById("play-button");
+const htmlRollButton = document.getElementById("roll-button");
+const htmlPlayArea   = document.getElementsByClassName("play-area");
+
+function hideElement(element) {
+    element.classList.toggle("show");
+}
+
+function showElement(element) {
+    
+}
+
+
+
+htmlPlayButton.addEventListener("click", );
+htmlRollButton.addEventListener();
